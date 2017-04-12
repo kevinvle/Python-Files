449ec794ccea585bf643cfa1533fbb91ee82567e
Kevin Viet Le <kevin1le@hotmail.com>
2017-04-11 17:21:35 -0700

    Swag

---
+++ magic8ball.py
@@ -0,0 +1,11 @@
+import random
+
+messages = ['It is certain',
+            'yes definitely', + \
+            'boo',
+            'swag',
+            'ask again later',
+            'not good',
+            'very doubtful']
+
+print(messages[random.randint(0, len(messages) - 1)])
